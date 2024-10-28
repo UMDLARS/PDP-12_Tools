a9dd194bd364ead2e798d1436b79ddea4903ae76
Zachary Kirtz <zach.kirtz@gmail.com>
2024-10-28 17:12:58 -0500

    Improve disk restore script to handle blank disks.

--- restore-disks-images.sh
+++ restore-disks-images.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 cd $(dirname $0)
-rm -rf disks
-cp -r unmodified-umd-disks
+#rm -rf disks
+mkdir disks
+cp unmodified-umd-disks/boot-pdp12.rk05 disks/
+cp unmodified-umd-disks/boot-simh.rk05 disks/
+cp unmodified-umd-disks/games.rk05 disks/
+cp unmodified-umd-disks/blank.rk05 disks/disk3.rk05
+cp unmodified-umd-disks/blank.rk05 disks/disk4.rk05
