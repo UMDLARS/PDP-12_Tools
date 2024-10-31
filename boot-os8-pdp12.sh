91c5b610038f6348136b98f1e8672c701042afec
Zachary Kirtz <zach.kirtz@gmail.com>
2024-10-31 13:14:28 -0500

    Add a script to launch os8diskserver and minicom for running os/8 on the pdp-12.

---
+++ boot-os8-pdp12.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TOOLS_ROOT_DIR=$(readlink -f $(dirname $0))
+cd $TOOLS_ROOT_DIR/diskserver-env/pdp12
+
+echo $TOOLS_ROOT_DIR
+
+# Run os8diskserver in the background.
+../../os8diskserver/SerialDisk/server/server -1 ../../disks/boot-pdp12.rk05 -2 ../../disks/games.rk05 -3 ../../disks/disk3.rk05 -4 ../../disks/disk4.rk05 >/dev/null 2>/dev/null &
+
+# Save disk server pid.
+SERVER_PID="$!"
+
+# Trap on exit to kill the disk server.
+trap 'kill "$SERVER_PID" 2> /dev/null' EXIT
+
+# Run minicom on ttyUSB1
+$TOOLS_ROOT_DIR/minicom/src/minicom -D /dev/ttyUSB1
