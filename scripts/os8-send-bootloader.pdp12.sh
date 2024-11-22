e319a4f951119ce962cd771ac6090e02546b235a
Zachary Kirtz <zach.kirtz@gmail.com>
2024-11-22 12:54:37 -0600

    scripts: os8-send-bootloader.pdp12.sh: Fix incorrect tools dir.

--- scripts/os8-send-bootloader.pdp12.sh
+++ scripts/os8-send-bootloader.pdp12.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TOOLS_ROOT_DIR=$(readlink -f $(dirname $0))
+TOOLS_ROOT_DIR=$(readlink -f $(dirname $0))/..
 
 # Setup ttyUSB1 the same way minicom does.
 stty -F /dev/ttyUSB1 ospeed 19200 ispeed 19200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke cstopb -ixon time 5
