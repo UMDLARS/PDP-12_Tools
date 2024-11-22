726676a5a89226a8f444eeaa5788d87db4f40c82
Zachary Kirtz <zach.kirtz@gmail.com>
2024-11-22 16:33:33 -0600

    scripts: minicom-update-config: Update translation table location.

--- scripts/minicom-update-config.sh
+++ scripts/minicom-update-config.sh
@@ -5,4 +5,4 @@ source $TOOLS_ROOT_DIR/scripts/detail/path-variables.sh
 
 # Copy minicom config to $HOME
 cp $RES_MINICOM_DATA_DIR/minirc.dfl $HOME/.minirc.PDP12
-echo "pu convf            $(pwd)/minicom-data/7bit-high8th.tbl" >> $HOME/.minirc.PDP12
+echo "pu convf            $RES_MINICOM_DATA_DIR/7bit-high8th.tbl" >> $HOME/.minirc.PDP12
