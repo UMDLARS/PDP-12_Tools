a0ceb9a4cf8080b242bdf9a1f96b6361068927ef
Zachary Kirtz <zach.kirtz@gmail.com>
2024-11-22 16:57:58 -0600

    scripts: Add a script to move/migrate the legacy disks directory to the modern/new disks directory.

---
+++ scripts/os8-migrate-legacy-disks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TOOLS_ROOT_DIR=$(readlink -f $(dirname $0))/..
+source $TOOLS_ROOT_DIR/scripts/detail/path-variables.sh
+
+if [ ! -d $LEGACY_DISKS_DIR ]; then
+    exit
+fi
+
+# Delete disks dir and move the legacy disks dir.
+rm -rf $DSKSRV_DISKS_DIR
+mv $LEGACY_DISKS_DIR $DSKSRV_DISKS_DIR
